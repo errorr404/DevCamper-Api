2c0b8855ec25c751c94e5e9eea156e1da8e0c4f7
errorr404 <dkbishwash@gmail.com>
2020-01-31 12:57:06 +0530

    connected to mongodb atlas server and created a global handler for promise rejection

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 const logger = require("./middleware/logger");
 const morgan = require("morgan");
 // bring the route file
@@ -8,6 +9,9 @@ const bootcamp = require("./routes/bootcamp");
 // load env variable in project
 dotenv.config({ path: "./config/config.env" });
 
+// connect to database
+connectDB();
+
 const app = express();
 /* 
 // use the middleware
@@ -15,8 +19,8 @@ app.use(logger);
 */
 
 // dev logging middleware
-if(process.env.NODE_ENV='development'){
-  app.use(morgan('dev'));
+if ((process.env.NODE_ENV = "development")) {
+  app.use(morgan("dev"));
 }
 
 // mount the route file to the app
@@ -25,7 +29,15 @@ app.use("/api/v1/bootcamps", bootcamp);
 const PORT = process.env.PORT || 5000;
 
 // start listning on the port
-app.listen(
+const server = app.listen(
   PORT,
   console.log(`server is up in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+// globale handler for unhandled promise rejections
+
+process.on("unhandledRejection", err => {
+  console.log(`Error: ${err.message}`);
+  // close server and exit process
+  server.close(() => process.exit(1));
+});
