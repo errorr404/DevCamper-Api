8307be8dc955d3353d9d7cd83da9a80da541d026
errorr404 <dkbishwash@gmail.com>
2020-01-31 18:41:34 +0530

    added seeder for inserting the data from file into db

---
+++ seeder.js
@@ -0,0 +1,56 @@
+/*
+ * Read the file first
+ * connect to the database
+ * import to the modal
+ */
+const fs = require("fs");
+const mongoose = require("mongoose");
+const colors = require("colors");
+const dotenv = require("dotenv");
+
+// Load env variable
+dotenv.config({ path: "./config/config.env" });
+
+// load models
+const Bootcamp = require("./models/Bootcamp");
+
+// connect to DB
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+});
+
+// Read json files
+const bootcamps = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/bootcamps.json`, "utf-8")
+);
+
+// Import into DB
+const importData = async () => {
+  try {
+    await Bootcamp.create(bootcamps);
+    console.log("Data Imported...".green.inverse);
+    process.exit();
+  } catch (err) {
+    console.error(err);
+  }
+};
+// Delete the DB
+const deleteData = async () => {
+  try {
+    await Bootcamp.deleteMany();
+    console.log("Data Destroyed...".red.inverse);
+    process.exit();
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// node seeder -i --> here argv[2] == -i
+if (process.argv[2] === "-i") {
+  importData();
+} else if (process.argv[2] === "-d") {
+  deleteData();
+}
